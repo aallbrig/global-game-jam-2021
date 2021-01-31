3c6640ac43aadd38383763521d0eafae589fda80
Andrew Allbright <aallbrig@gmail.com>
2021-01-31 16:46:33 -0500

    Blockbuster employee has quest marker above head

---
+++ Global Game Jam 2021/Assets/Scripts/MonoBehaviours/Customizers/ConditionalInstantiate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace MonoBehaviours.Customizers
+{
+    public class ConditionalInstantiate : MonoBehaviour
+    {
+        public BoolVar condition;
+        public GameObject trueObject;
+        public GameObject falseObject;
+        private GameObject _instance;
+        private bool _valueAtInstantiate;
+        private Transform _transform;
+
+        private void InstantiateGameObject(GameObject desiredGameObject)
+        {
+            if (_instance != null) Destroy(_instance);
+            _instance = Instantiate(desiredGameObject, _transform);
+            _valueAtInstantiate = condition.value;
+        }
+
+        private void EvaluateBoolVar()
+        {
+            if (_valueAtInstantiate != condition.value)
+                InstantiateGameObject(condition.value ? trueObject : falseObject);
+        }
+
+        private void Update() => EvaluateBoolVar();
+
+        private void Awake()
+        {
+            _transform = transform;
+            InstantiateGameObject(condition.value ? trueObject : falseObject);
+        }
+    }
+}
