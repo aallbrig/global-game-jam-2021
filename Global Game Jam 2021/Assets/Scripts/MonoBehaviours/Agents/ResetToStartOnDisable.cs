877cacfb40f926bad5c6b24cf72aece9daa94324
Andrew Allbright <aallbrig@gmail.com>
2021-01-31 15:38:04 -0500

    Player, collectibles, and player inventory is cleared on game restart

---
+++ Global Game Jam 2021/Assets/Scripts/MonoBehaviours/Agents/ResetToStartOnDisable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace MonoBehaviours.Agents
+{
+    public class ResetToStartOnDisable : MonoBehaviour
+    {
+        public Transform startingPosition;
+        private Transform _transform;
+
+        private void Awake() => _transform = transform;
+
+        private void OnDisable()
+        {
+            _transform.position = startingPosition.position;
+            _transform.rotation = startingPosition.rotation;
+        }
+    }
+}
---
+++ Global Game Jam 2021/Assets/Scripts/ScriptableObjects/FiniteStateMachines/GameState/Actions/SetGameObjectRuntimeListActive.cs
@@ -0,0 +1,18 @@
+﻿using MonoBehaviours.Controllers;
+using ScriptableObjects.RuntimeSets;
+using UnityEngine;
+
+namespace ScriptableObjects.FiniteStateMachines.GameState.Actions
+{
+    [CreateAssetMenu(fileName = "new set game object runtime list active", menuName = "GGJ2021/GameState/Actions/SetGameObjectRuntimeListActive", order = 0)]
+    public class SetGameObjectRuntimeListActive : Action
+    {
+        public GameObjectRuntimeSet gameObjectRuntimeSet;
+        public bool activeValue;
+
+        public override void Act(GameStateController controller)
+        {
+            gameObjectRuntimeSet.list.ForEach(gameObject => gameObject.SetActive(activeValue));
+        }
+    }
+}
